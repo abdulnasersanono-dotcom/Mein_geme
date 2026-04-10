1b846e6867120a4ba4127ccd54a8ea890cc87403
abdulnasersanono-dotcom <abdulnasersanono@gmail.com>
2026-04-10 09:39:37 +0000

    up

--- js/screens/monopoly.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
- /* Complete Monopoly game code from the inline script - paste the full JS here */
-
- /* Paste the entire script content from the inline <script> tag in index.html */
-
- /* Including all globals G, BOARD_DATA, functions initGame, startTurn, doRollDice, etc. */
-
